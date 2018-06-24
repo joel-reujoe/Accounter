78af2bca3457026835f54ebe712fb6c345e01217
unknown <joel.reujoe@gmail.com>
2018-06-25 02:26:52 +0530

    joel

--- controller.js
+++ controller.js
@@ -27,6 +27,7 @@ app.use(function (req, res, next) {
 //         next();
 //     });
 // });
+app.use(express.static(path.join(__dirname,'front-end')))
 var path=require('path');
 app.get('/addResident', (req, res) => {
     console.log(req.query);
