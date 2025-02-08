70fed08c0271a7bca6784ab27ac23c7413e8ccfa
root <joel.reujoe@gmail.com>
2025-02-08 12:47:33 +0530

    fixed packages

--- functions/src/switch/switch_router.ts
+++ functions/src/switch/switch_router.ts
@@ -14,47 +14,47 @@ class switch_router
         var action=req.body.action;
         var ctrl_account_object=new ctrl_account();
         // console.log(req.body);
+        var data;
         switch(action)
         {
             case "authenticate":
-                var data=await ctrl_account_object.ctrl_functions.authenticate(req,next);
+                data=await ctrl_account_object.ctrl_functions.authenticate(req,next);
                 res.send(data)
                 break;
             case "addSociety":
-                var data=await ctrl_account_object.ctrl_functions.addSociety(req,next);
+                data=await ctrl_account_object.ctrl_functions.addSociety(req,next);
                 res.send(data);
                 break
             case "viewSociety":
-            var data=await ctrl_account_object.ctrl_functions.viewSociety(req,next);
+                data=await ctrl_account_object.ctrl_functions.viewSociety(req,next);
             res.send(data);
                 break;
             case "getSociety":
-                var data=await ctrl_account_object.ctrl_functions.getSociety(req,next);
+                data=await ctrl_account_object.ctrl_functions.getSociety(req,next);
                 res.send(data);
                 break;
             case "addResident":
-                var data=await ctrl_account_object.ctrl_functions.addResident(req,next);
+                data=await ctrl_account_object.ctrl_functions.addResident(req,next);
                 res.send(data);                
                 break
             case "viewResident":
-                var data=await ctrl_account_object.ctrl_functions.viewResident(req,next);
+                data=await ctrl_account_object.ctrl_functions.viewResident(req,next);
                 res.send(data);                                
                 break;
             case "getResident":
-                var data=await ctrl_account_object.ctrl_functions.getResident(req,next);
+                data=await ctrl_account_object.ctrl_functions.getResident(req,next);
                 res.send(data);
                 break;
             case "createBill":
-                var data=await ctrl_account_object.ctrl_functions.createBill(req,next);
+                data=await ctrl_account_object.ctrl_functions.createBill(req,next);
                 res.send(data);
                 break;
             case "viewBill":
-                console.log("hi")
-                var data=await ctrl_account_object.ctrl_functions.viewBill(req,next);
+                data=await ctrl_account_object.ctrl_functions.viewBill(req,next);
                 res.send(data);
                 break;
             case "viewAll":
-                var data=await ctrl_account_object.ctrl_functions.viewAll(req,next)
+                data=await ctrl_account_object.ctrl_functions.viewAll(req,next)
                 res.send(data);
                 break;
             default:
